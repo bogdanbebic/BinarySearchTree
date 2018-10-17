178b09200c16ec9c6758b1701f94c351e5de6fb7
bogdanbebic <bb170011d@student.etf.bg.ac.rs>
2018-10-17 16:27:07 +0200

    Implemented search and insert in BST

--- BinarySearchTree/BinarySearchTree/Source.cpp
+++ BinarySearchTree/BinarySearchTree/Source.cpp
@@ -7,13 +7,13 @@ struct BinarySearchTree {
 		TreeNode *left, *right;
 		TreeNode(int node_key = 0, TreeNode *left_node_ptr = nullptr, TreeNode *right_node_ptr = nullptr);
 	};
-
 	TreeNode *root;
-
 	BinarySearchTree(TreeNode *root_node = nullptr);
-	
+	TreeNode * search(int key);
+	void insert(TreeNode *new_node);
+
 	// TODO: implementation
-	
+
 };
 
 BinarySearchTree::TreeNode::TreeNode(int node_key, TreeNode* left_node_ptr, TreeNode* right_node_ptr) {
@@ -24,6 +24,51 @@ BinarySearchTree::TreeNode::TreeNode(int node_key, TreeNode* left_node_ptr, Tree
 
 BinarySearchTree::BinarySearchTree(TreeNode *root_node) {
 	root = root_node;
+}
+
+BinarySearchTree::TreeNode* BinarySearchTree::search(int key) {
+	TreeNode *current = root;
+	while (current != nullptr && key != current->key) {
+		if (key < current->key) {
+			current = current->left;
+		}
+		else {
+			current = current->right;
+		}
+	}
+	return current;
+}
+
+void BinarySearchTree::insert(TreeNode *new_node) {
+	TreeNode *current = nullptr, *next = root;
+	if (root == nullptr) {
+		root = new_node;
+		return;
+	}
+	while (next != nullptr) {
+		current = next;
+		if (new_node->key == current->key) {
+			// THIS WHILE STATEMENT ENSURES FIFO
+			while (current->left != nullptr && current->left->key == new_node->key) {
+				current = current->left;
+			}
+			break;
+		}
+		if (new_node->key < current->key) {
+			next = current->left;
+		}
+		else {
+			next = current->right;
+		}
+	}
+	if (new_node->key <= current->key) {
+		new_node->left = current->left;
+		current->left = new_node;
+	}
+	else {
+		new_node->right = current->right;
+		current->right = new_node;
+	}
 }
 
 
